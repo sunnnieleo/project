9ca9b55020b23e7699659297672e5eb8730ab924
Sonya Stuhec-Leonard <sonya.stuhec.leonard@gmail.com>
2018-04-06 14:18:30 -0400

    incorperating Fc - runs - not as desired

--- project.m
+++ project.m
@@ -2,7 +2,8 @@
 
 %define electron parameters
 mass = 9.109E-31; %in kg
-charge = -1602E-19; %in C
+chargeN = -1602E-19; %in C
+chargeP = 1602E-19; %in C
 TauMN = 0.2E-12;
 
 k = physconst('Boltzmann'); %Use of constants in matlab
@@ -12,7 +13,7 @@ T = 300; % temperature in Kalvin
 %https://en.wikipedia.org/wiki/Thermal_velocity)
 v_th = sqrt(k*T/mass);
 
-numP = 100; %number of particles
+numP = 20; %number of particles
 
 %box definitions
 xmax = 75;
@@ -29,13 +30,21 @@ t = (200e-9/v_th)/100;
 %particle initalization
 xmaxNano = 75e-9;
 ymaxNano = 50e-9;
-
+ 
+%  %conductiity equation
+%  condEq = charge^2*TauMN*numP/mass;
+%  background = 1;
+ 
+ %initalize regions
+ [conductivtyN, posN, velN] = regionInit( 'electron', numP);
+ [conductivtyP, posP, velP] = regionInit( 'hole', numP);
+ 
 
 %% Main loops for producing the "movie" of particles
 
 
 %Probability of scattering
-ProbScat = 1- exp(-t/TauMN);
+ProbScat = 1; %1- exp(-t/TauMN);
 scatterTime = zeros(numP, 1);
 ProbMoveWhenHappy = 0.1;
 iterations = 20;
@@ -54,13 +63,13 @@ for iter =1:iterations
     
     %Probability of electrons scattering - should it move and how much?
     
-    velN = ShouldItMove( t, posN, charge, center, ProbScat, ProbMoveWhenHappy, velN, mass);
-    velP = ShouldItMove( t, posP, charge, center, ProbScat, ProbMoveWhenHappy, velP, mass);
+    velN = ShouldItMove( t, posN, chargeN, center, ProbScat, ProbMoveWhenHappy, velN, mass, posP, chargeP);
+    velP = ShouldItMove( t, posP, chargeP, center, ProbScat, ProbMoveWhenHappy, velP, mass, posN, chargeN);
     
     %Boundary conditions
     
-    posN = updatePosition( numP, posN, velN, t, xmin, xmaxNano, ymin, ymaxNano);
-    posP = updatePosition( numP, posP, velP, t, xmin, xmaxNano, ymin, ymaxNano);
+    posN = updatePosition(v_th, numP, posN, velN, t, xmin, xmaxNano, ymin, ymaxNano, chargeN);
+    posP = updatePosition(v_th, numP, posP, velP, t, xmin, xmaxNano, ymin, ymaxNano, chargeP);
     
     
     
@@ -69,27 +78,27 @@ for iter =1:iterations
     plot(posN(:, 1), posN(:, 2), '.b')
     hold on
     plot(posP(:, 1), posN(:, 2), '.r')
-    hold off
-    pause(0.2)
+%     hold off
+    pause(0.5)
     title ('Simulation of Electron Trajectories')
     
     %Plot updated conductivity fo rmoving electrons in each resion
     
     CondMapUpdated = ConductivityCal(posN, 'electron', numP);
     
-    figure(5)
-    subplot(1, 2, 1)
-    surf(CondMapUpdated)
-    title ('Conductivity map changes with time')
-    colorbar
-    subplot(1, 2, 2)
-    plot(posN(1:length(posN), 1), posN(1:length(posN), 2), '.b')
-    hold on
-    plot(posP(1:length(posP), 1), posN(1:length(posP), 2), '.r')
-    axis([xmin, xmaxNano, ymin, ymaxNano])
-    pause(0.2)
-    title ('Simulation of Electron Trajectories')
-    
+%     figure(5)
+%     subplot(1, 2, 1)
+%     surf(CondMapUpdated)
+%     title ('Conductivity map changes with time')
+%     colorbar
+%     subplot(1, 2, 2)
+%     plot(posN(1:length(posN), 1), posN(1:length(posN), 2), '.b')
+%     hold on
+%     plot(posP(1:length(posP), 1), posN(1:length(posP), 2), '.r')
+%     axis([xmin, xmaxNano, ymin, ymaxNano])
+%     pause(0.2)
+%     title ('Simulation of Electron Trajectories')
+%     
     
     %temperature map
     %convert velocieites into temperatures, then use hist3 to bin and plot
--- updatePosition.m
+++ updatePosition.m
@@ -1,33 +1,44 @@
-function [ positions ] = updatePosition( numP, positions, velocity, t, xmin, xmax, ymin, ymax)
+function [ positions ] = updatePosition( vth, numP, positions, velocity, t, xmin, xmax, ymin, ymax, charge)
 %updatePosition Summary of this function goes here
 %   Detailed explanation goes here
 
 %Boundary conditions
 for j=1:2
     for k=1:numP
-        revx(k) = 0;
-        %move particles a each time step based on their velocity
-        Prevposition = positions(k, j);
+        Prevposition = positions(k, :);
+        
+        %move particles by randome velocit generated in ShouldItMove
         positions(k, j) = positions(k, j) + velocity(k, j)*t;
         
-        %restrications of x-cordinate of each particle BC
-        if j == 1 %(for all x cordinates)
-            if positions(k, 1) <= xmin || positions(k, 1)>= xmax
-                
-                positions(k, j) = Prevposition; 
-                
-                revx(k) = 1;
+        %             %restrications of x-cordinate of each particle BC
+        %             if j == 1 %(for all x cordinates)
+        %                 if positions(k, 1) <= xmin
+        %
+        %                     positions(k, 1) = Prevposition(1, 1) + vth*t;
+        %                 end
+        %                 if positions(k, 1)>= xmax
+        %                     positions(k, 1) = Prevposition(1, 1) - vth*t;
+        %                 end
+        %             end
+        %
+        %             if j == 2
+        %                 if positions(k, 2) <= ymin
+        %                     positions(k, 2) = Prevposition(1, 2)  + vth*t;;
+        %                 end
+        %                 if  positions(k, 2) >= ymax %---&& %engery less than required to penetrate
+        %                     positions(k, 2) = Prevposition(1, 2)  - vth*t;;
+        %                 end
+        %             end
+        
+        
+        if positions(k, 1) <= xmin || positions(k, 1)>= xmax || positions(k, 2) <= ymin || positions(k, 2) >= ymax
+            %generate new position within bounds
+            if charge > 0
+            [positions(k, :), velocity(k, :) ] = randInit( 1, xmax/2, xmax, ymax, vth );
+            else
+                [positions(k, :), velocity(k, :) ] = randInit( 1, xmin, xmax/2, ymax, vth );
             end
         end
-        
-        if j == 2
-            if (positions(k, 2) <= ymin || positions(k, 2) >= ymax) %---&& %engery less than required to penetrate
-                positions(k, j) = Prevposition; 
-                
-            end
-        end
-        
     end
-end
 end
 
